b25cbdbe640ea9519f674e9078bd9e485dfa464d
Trofymenko Vladyslav <naughtysora@proton.me>
2025-09-21 19:49:34 +0300

    has edges

--- test/index.js
+++ test/index.js
@@ -4,14 +4,14 @@ const Graph = require('../main.js');
 const { describe, it } = require('node:test');
 const assert = require('node:assert');
 
-
 describe('graph', () => {
   describe('undirected', () => {
-    it('add/edge', () => {
-      const graph = new Graph();
-      graph.add('a').add('b').add('c');
-      graph.edge('a', 'b');
-      graph.edge('c', 'a');
+    const graph = new Graph();
+    graph.add('a').add('b').add('c').add('e');
+    graph.edge('a', 'b');
+    graph.edge('c', 'a');
+
+    it('get edges', () => {
       assert.deepStrictEqual(
         graph.getOutEdges('c').map(item => item.value),
         ['a']
@@ -21,6 +21,13 @@ describe('graph', () => {
         graph.getInEdges('c').map(item => item.value),
         ['a', 'b']
       );
+    });
+
+    it('has edges', () => {
+      assert.strictEqual(graph.hasInEdges('c', 'b'), true);
+      assert.strictEqual(graph.hasInEdges('c', 'e'), false);
+      assert.strictEqual(graph.hasOutEdges('c', 'a'), true);
+      assert.strictEqual(graph.hasOutEdges('c', 'e'), false);
     });
   });
 });
