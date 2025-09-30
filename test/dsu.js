a7562b336a223a613b4ba8b9000d4023dfd3d959
Trofymenko Vladyslav <naughtysora@proton.me>
2025-09-30 22:22:28 +0300

    dsu, tests

--- test/dsu.js
+++ test/dsu.js
@@ -1,18 +1,61 @@
 'use strict';
 
-const { describe, it } = require('node:test');
+const { describe, it, beforeEach } = require('node:test');
 const assert = require('node:assert');
 const DSU = require('../lib/DSU.js');
 
-describe.only('DSU', () => {
+describe.only('DSU', async (t) => {
+  let dsu = new DSU();
+
+  beforeEach(() => {
+    dsu = new DSU();
+    dsu.add('a').add('b').add('c').add('d').add('e');
+  });
+
   it('add', () => {
-    const dsu = new DSU();
-    dsu.add('33').add(1).add(2).add(3).add(4);
-    dsu.union(1, '33');
-    dsu.union(2, '33');
-    dsu.union(3, 2);
-    dsu.debug();
-    console.log(dsu.root(3));
-    dsu.debug();
+    assert.strictEqual(dsu.root('a'), 0);
+    assert.strictEqual(dsu.root('b'), 1);
+    assert.strictEqual(dsu.root('c'), 2);
+    assert.strictEqual(dsu.root('d'), 3);
+    assert.strictEqual(dsu.root('e'), 4);
+  });
+
+  it('union', () => {
+    dsu.union('a', 'b');
+    dsu.union('c', 'd');
+    assert.strictEqual(dsu.connected('a', 'b'), true);
+    assert.strictEqual(dsu.connected('c', 'd'), true);
+    assert.strictEqual(dsu.connected('a', 'c'), false);
+    dsu.union('b', 'c');
+    assert.strictEqual(dsu.connected('a', 'e'), false);
+  });
+
+  it('size', () => {
+    dsu.union('a', 'b');
+    dsu.union('c', 'd');
+    dsu.union('b', 'c');
+    assert.strictEqual(dsu.size('a'), 4);
+    assert.strictEqual(dsu.size('b'), 4);
+    assert.strictEqual(dsu.size('c'), 4);
+    assert.strictEqual(dsu.size('d'), 4);
+    assert.strictEqual(dsu.size('e'), 1);
+  });
+
+  it('root', () => {
+    dsu.union('a', 'b');
+    dsu.union('c', 'd');
+    dsu.union('b', 'c');
+    const rootBefore = dsu.root('d');
+    dsu.root('d');
+    const rootAfter = dsu.root('d');
+    assert.strictEqual(rootBefore, rootAfter);
+  });
+
+  it('find', () => {
+    assert.strictEqual(dsu.find(dsu.root('a')), 'a');
+    assert.strictEqual(dsu.find(dsu.root('b')), 'b');
+    dsu.union('a', 'b');
+    assert.strictEqual(dsu.find(dsu.root('a')), 'a');
+    assert.strictEqual(dsu.find(dsu.root('b')), 'a');
   });
 });
